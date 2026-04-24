0466f24be6a8a3d2587dd6d8ed6c74e315202a9b
XcgsdV <josepht0615@gmail.com>
2026-04-24 13:26:58 -0500

    Completed v3

--- scripts/exp.sh
+++ scripts/exp.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# make cpp/build the working directory so directory references aren't confusing wrt 
+# where the bash is and where the cpp is
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 cd "$SCRIPT_DIR/../cpp/build" 
 
@@ -9,32 +11,13 @@ EXPERIMENT_DIR="${TIMESTAMP}"
 
 mkdir -p "${DATA_DIR}/${EXPERIMENT_DIR}"/{sphere,torus,euclidean}
 
+
 echo "Created: ${EXPERIMENT_DIR}/{sphere,torus,euclidean}"
 
-
-#for manifold in sphere torus euclidean; do
-#    for diffusion in 0.05 0.1 0.5 1.0 5.0; do
-#        for mobility in 0.1 1.0 5.0 10.0; do
-#            for seed in 1 2 3; do
-#                outfile="_d${diffusion}_m${mobility}_s${seed}.h5"
-#                echo "Running: $outfile"
-#                ./core \
-#                    --manifold $manifold \
-#                    --diffusion $diffusion \
-#                    --mobility $mobility \
-#                    --seed $seed \
-#                    --output $outfile \
-#                    --expdir $EXPERIMENT_DIR
-#            done
-#        done
-#    done
-#done
-
-
 for manifold in sphere torus euclidean; do
-    for diffusion in 0.05; do
-        for mobility in 0.1; do
-            for seed in 1 2; do
+    for diffusion in 0.05 0.1 0.5 1.0 5.0; do
+        for mobility in 0.1 1.0 5.0 10.0; do
+            for seed in 1 2 3; do
                 outfile="d${diffusion}_m${mobility}_s${seed}.h5"
                 echo "Running: ${manifold} ${outfile}"
                 ./core \
